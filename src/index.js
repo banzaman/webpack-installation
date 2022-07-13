9afabc177391122380e32a6f9d841d3d1eab7a9b
Mark Rubanza <70164878+banzaman@users.noreply.github.com>
2022-07-13 20:50:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
   
-    
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
   }
